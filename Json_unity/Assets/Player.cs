75b54b6adc82fa0bcc608533576d410f3adb9ea4
WislonLin <p96091123@gs.ncku.edu.tw>
2021-03-19 18:26:26 +0800

    Version_01

---
+++ Json_unity/Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public  class Player : MonoBehaviour
+{
+     // Playerstate myState = new Playerstate (); 
+}
+public  class Playerstate
+{
+	public string name;
+	public int level;
+	public Vector3 pos;
+}
+
+
+
---
+++ Json_unity/Assets/levelUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class levelUp:Player //繼承Player
+{
+   private int level;
+   private Playerstate loadData;
+
+	void Start()
+	{
+		StreamReader file = new StreamReader (System.IO.Path.Combine(Application.streamingAssetsPath,"myState.json")); 
+		string loadJson = file.ReadToEnd ();
+		file.Close ();
+		loadData = new Playerstate();
+		loadData = JsonUtility.FromJson<Playerstate> (loadJson);
+	}
+	public void LevelUpbtn()
+	{
+	    Playerstate myState = new Playerstate ();
+		level = loadData.level;
+		level++;
+		loadData.level = level;
+		Debug.Log (loadData.level);
+		string saveString = JsonUtility.ToJson (loadData); //將MyState轉換成Json格式的字串
+		//Application.streamingAssetsPath檔案路徑
+		StreamWriter file2=new StreamWriter(System.IO.Path.Combine(Application.streamingAssetsPath,"myState.json"));//將字串存到硬碟
+		file2.Write (saveString);
+		file2.Close ();
+	}
+}
